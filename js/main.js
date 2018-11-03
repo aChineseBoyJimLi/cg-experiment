ab717a960a9d7a19c3aa5b50d1a33910f20adf0b
unknown <lindie>
2018-11-03 13:49:07 +0800

    “修改了一点点”

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-
-// var canvas = document.getElementById('myCanvas');
-
-
-$(function(){
-    // InitGrid();
-    
-    // var p1 = new Point(1,2).TransformAxis();
-    // var p = new Pixel(pixelWidth,p1.pX,p1.pY);
-    // p.FillPixel("#cccccc");
-})
-
 function Draw(){
     var p = new Point(0,0);
     p = p.TransformAxis();
